e36c66a5a8c319accc4eb5e0414cb7f750d94394
andy--rose <arose@medallia.com>
2015-10-30 16:40:18 -0700

    Reorg for multi-processing

--- Shield/Shield.ino
+++ Shield/Shield.ino
@@ -1,170 +1,84 @@
-                                                                                                                  #include <Adafruit_NeoPixel.h>
-#ifdef __AVR__
-  #include <avr/power.h>
-#endif
+#include <Adafruit_NeoPixel.h>
+#include "ShieldPattern.h"
 
 #define PIN_SMALL 3
+#define PIN_LARGE 1
 #define LEDS_SMALL 9
+#define LEDS_LARGE 16
 #define MIN_CHASE_SPEED 60
-#define MAX_CHASE_SPEED 20
-#define CHASE_SPEED_INTERVAL 1
+#define MAX_CHASE_SPEED 30
+#define CHASE_SPEED_INTERVAL 2
 
-// Parameter 1 = number of pixels in strip
-// Parameter 2 = Arduino pin number (most are valid)
-// Parameter 3 = pixel type flags, add together as needed:
-//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
-//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
-//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
-//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(LEDS_SMALL, PIN_SMALL, NEO_GRB + NEO_KHZ800);
+ShieldPattern small_circle = ShieldPattern(LEDS_SMALL, PIN_SMALL, NEO_GRB + NEO_KHZ800, &SmallCircleComplete);
+ShieldPattern large_circle = ShieldPattern(LEDS_LARGE, PIN_LARGE, NEO_GRB + NEO_KHZ800, &LargeCircleComplete);
 
-// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
-// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
-// and minimize distance between Arduino and first pixel.  Avoid connecting
-// on a live circuit...if you must, connect GND first.
+bool speedChange = false;
+bool accelerating_small = true;
+bool accelerating_large = true;
 
+//Adafruit_NeoPixel strip_small = Adafruit_NeoPixel(LEDS_SMALL, PIN_SMALL, NEO_GRB + NEO_KHZ800);
+//Adafruit_NeoPixel strip_large = Adafruit_NeoPixel(LEDS_LARGE, PIN_LARGE, NEO_GRB + NEO_KHZ800);
 void setup() {
-  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-  #if defined (__AVR_ATtiny85__)
-    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-  #endif
-  // End of trinket special code
+  small_circle.begin();
+  large_circle.begin();
 
-
-  strip.begin();
-  strip.show(); // Initialize all pixels to 'off'
+  small_circle.RainbowCycle(1);
+  large_circle.RainbowCycle(1);
+//  small_circle.TheaterChase(small_circle.Color(255,255,0), small_circle.Color(0,0,50), 100);
+//  large_circle.TheaterChase(large_circle.Color(255,255,0), large_circle.Color(0,0,50), 100);
 }
 
 void loop() {
-  // Some example procedures showing how to display to the pixels:
-//  colorWipe(strip.Color(255, 0, 0), 50); // Red
-//  colorWipe(strip.Color(0, 255, 0), 50); // Green
-//  colorWipe(strip.Color(0, 0, 255), 50); // Blue
-  // Send a theater pixel chase in...
-  chaseCycle(strip.Color(127, 127, 127));
-//  theaterChase(strip.Color(127, 0, 0), 50); // Red
-//  theaterChase(strip.Color(0, 0, 127), 50); // Blue
-
-//  rainbow(20);
-//  rainbowCycle(20);
-//  theaterChaseRainbow(50);
+  small_circle.Update();
+  large_circle.Update();
 }
 
-// Fill the dots one after the other with a color
-void colorWipe(uint32_t c, uint8_t wait) {
-  for(uint16_t i=0; i<strip.numPixels(); i++) {
-    strip.setPixelColor(i, c);
-    strip.show();
-    delay(wait);
-  }
-}
-
-void rainbow(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-// Slightly different, this makes the rainbow equally distributed throughout
-void rainbowCycle(uint8_t wait) {
-  uint16_t i, j;
-
-  for(j=0; j<256*5; j++) { // 5 cycles of all colors on wheel
-    for(i=0; i< strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel(((i * 256 / strip.numPixels()) + j) & 255));
-    }
-    strip.show();
-    delay(wait);
-  }
-}
-
-//Theatre-style crawling lights.
-void theaterChase(uint32_t c, uint8_t wait) {
-  for (int j=0; j<10; j++) {  //do 10 cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, c);    //turn every third pixel on
+void SmallCircleComplete()
+{
+  if (speedChange)
+  {
+    if (accelerating_small)
+    {
+      small_circle.Interval -= CHASE_SPEED_INTERVAL;
+      if (small_circle.Interval < MAX_CHASE_SPEED)
+      {
+        accelerating_small = false;
+        small_circle.Interval += CHASE_SPEED_INTERVAL;
       }
-      strip.show();
-
-      delay(wait);
-
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+    }
+    else
+    {
+      small_circle.Interval += CHASE_SPEED_INTERVAL;
+      if (small_circle.Interval > MIN_CHASE_SPEED)
+      {
+        accelerating_small = true;
+        small_circle.Interval-= CHASE_SPEED_INTERVAL;
       }
     }
   }
 }
 
-void chaseAcceleration(uint32_t c) {
-  for (int s=MIN_CHASE_SPEED; s>MAX_CHASE_SPEED; s--) {
-    chase(c, s);
-  }
-}
-
-void chaseBrake(uint32_t c) {
-  for (int s=MAX_CHASE_SPEED; s<MIN_CHASE_SPEED; s++) {
-    chase(c, s);
-  }
-}
-
-void chaseCycle(uint32_t c) {
-  chaseAcceleration(c);
-  chaseBrake(c);
-}
-
-//Theatre-style crawling lights.
-void chase(uint32_t c, uint32_t wait) {
-  for (int j=0; j<CHASE_SPEED_INTERVAL; j++) {  //do n cycles of chasing
-    for (int q=0; q < 3; q++) {
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, c);    //turn every third pixel on
+void LargeCircleComplete()
+{
+  if (speedChange)
+  {
+    if (accelerating_large)
+    {
+      large_circle.Interval -= CHASE_SPEED_INTERVAL;
+      if (large_circle.Interval < MAX_CHASE_SPEED)
+      {
+        accelerating_large = false;
+        large_circle.Interval += CHASE_SPEED_INTERVAL;
       }
-      strip.show();
-      delay(wait);
-
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
+    }
+    else
+    {
+      large_circle.Interval += CHASE_SPEED_INTERVAL;
+      if (large_circle.Interval > MIN_CHASE_SPEED)
+      {
+        accelerating_large = true;
+        large_circle.Interval -= CHASE_SPEED_INTERVAL;
       }
     }
   }
-}
-
-//Theatre-style crawling lights with rainbow effect
-void theaterChaseRainbow(uint8_t wait) {
-  for (int j=0; j < 256; j++) {     // cycle all 256 colors in the wheel
-    for (int q=0; q < 3; q++) {
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, Wheel( (i+j) % 255));    //turn every third pixel on
-      }
-      strip.show();
-
-      delay(wait);
-
-      for (int i=0; i < strip.numPixels(); i=i+3) {
-        strip.setPixelColor(i+q, 0);        //turn every third pixel off
-      }
-    }
-  }
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
 }
